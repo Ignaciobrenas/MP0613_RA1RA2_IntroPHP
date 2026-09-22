042c893f34ee34131e1a2dddab59d2af229c4274
Ignaciobrenas <ignaciobrenas@gmail.com>
2026-09-22 18:20:12 +0200

    ibrenas/P26 Complete #26

--- exercises/P26_Same.php
+++ exercises/P26_Same.php
@@ -5,6 +5,19 @@ class P26_Same
     public function main(): void
     {
         // Write your code here
-       
+
+        echo "Enter the first string:\n";
+        $string1 = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+
+        echo "Enter the second string:\n";
+        $string2 = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+
+        if ($string1 === $string2) {
+            echo "Same\n";
+
+        } else {
+            echo "Different";
+        }
+        
     }
 }
