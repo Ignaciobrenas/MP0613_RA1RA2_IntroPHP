69f1705e00b66eadd263e4efd048709126fcfa0b
Ignaciobrenas <ignaciobrenas@gmail.com>
2026-09-22 18:04:26 +0200

    ibrenas/P25 Complete #25

--- exercises/P25_Password.php
+++ exercises/P25_Password.php
@@ -5,6 +5,19 @@ class P25_Password
     public function main(): void
     {
         // Write your code here
-       
+        //se escribe la contraseña
+        echo "Password?\n";
+
+        $password = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+
+        if ($password === "Caput Draconis") {
+            echo "Welcome!\n";
+        } else {
+
+           echo "Off with you!\n";
+        }
+
+
+        
     }
 }
