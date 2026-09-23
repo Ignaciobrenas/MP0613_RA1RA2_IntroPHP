0dd2e3100429f713f9bc29906fac8746392c036d
Ignaciobrenas <ignaciobrenas@gmail.com>
2026-09-23 16:40:28 +0200

    ibrenas/P28 Complete #28

--- exercises/P28_LeapYear.php
+++ exercises/P28_LeapYear.php
@@ -5,6 +5,13 @@ class P28_LeapYear
     public function main(): void
     {
         // Write your code here
-       
+        echo "Give a year: ";
+        $year = (int) trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+
+        if (($year % 4 === 0 && $year % 100 !== 0) || ($year % 400 === 0)) {
+            echo "The year is a leap year.\n";
+        } else {
+            echo "The year is not a leap year.\n";
+        }
     }
 }
